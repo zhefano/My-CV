efb381000819c4acb401d1318a7e16d506a90b5f
zhefano <cubza@protonmail.com>
2025-01-13 22:23:21 +0100

    uppdaterar

--- src/components/contact/Contact.jsx
+++ src/components/contact/Contact.jsx
@@ -1,13 +1,6 @@
 import React from 'react'
 
 export default function Contact() {
-  
-  const handleSubmit = (e) => {
-    e.preventDefault()
-    
-    console.log('Formulär skickat!')
-  }
-
   return (
     <section
       id="contact"
@@ -19,44 +12,24 @@ export default function Contact() {
         <h2 className="mb-4 text-4xl tracking-tight font-extrabold text-center text-purple-400">
           Kontakta mig
         </h2>
-        <form onSubmit={handleSubmit} className="space-y-8">
-          <div>
-            <label
-              htmlFor="email"
-              className="block mb-2 text-xl font-medium text-purple-300"
-            >
-              Email
-            </label>
-            <input
-              type="email"
-              id="email"
-              className="shadow-sm bg-gray-800 font-bold border border-gray-600 text-white text-sm rounded-lg focus:ring-purple-500 focus:border-purple-500 block w-full p-2.5"
-              placeholder="name@example.com"
-              required
-            />
-          </div>
-          <div className="sm:col-span-2">
-            <label
-              htmlFor="message"
-              className="block mb-2 text-xl font-medium text-purple-300"
-            >
-              Meddelande
-            </label>
-            <textarea
-              rows="6"
-              id="message"
-              className="shadow-sm bg-gray-800 font-bold border border-gray-600 text-white text-sm rounded-lg focus:ring-purple-500 focus:border-purple-500 block w-full p-2.5"
-              placeholder="Skriv ditt meddelande..."
-              required
-            />
-          </div>
-          <button
-            type="submit"
-            className="inline-flex text-white bg-purple-600 border-0 py-2 px-6 focus:outline-none hover:bg-purple-700 hover:shadow-[0_0_40px_rgba(128,0,128,0.7)] rounded-full text-lg"
+        <p className="text-center text-xl text-purple-300 mb-8">
+          Du kan kontakta mig via{' '}
+          <a
+            href="https://www.linkedin.com/in/noel-gill-136763306"
+            target="_blank"
+            rel="noopener noreferrer"
+            className="text-purple-400 underline"
           >
-            Skicka meddelande
-          </button>
-        </form>
+            LinkedIn
+          </a>{' '}
+          eller mejla mig på{' '}
+          <a
+            href="mailto:noelblom98@hotmail.com"
+            className="text-purple-400 underline"
+          >
+            noelblom98@hotmail.com
+          </a>.
+        </p>
       </div>
     </section>
   )
